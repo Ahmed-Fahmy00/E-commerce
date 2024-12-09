eecf550441c089efd5bdbdf5bdd4e599603182f4
Ahmed Mohamed Fahmy Eldawy Hamed <185392994+Ahmed-Fahmy00@users.noreply.github.com>
2024-12-09 14:31:58 +0200

    3rd commit

--- Project_MS1/src/main/Main.java
+++ Project_MS1/src/main/Main.java
@@ -5,7 +5,7 @@ import service.CustomerService;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         MainMenu();
     }
